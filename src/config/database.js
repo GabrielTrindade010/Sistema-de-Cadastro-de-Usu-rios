75219dbe7477869a2173fcc7f130be04709fe845
GabrielTrindade <gabrieldiastrin63@gmail.com>
2026-02-28 21:59:28 -0300

    Configurando conexão com o banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,17 @@ require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, 
     process.env.DB_USERNAME, 
-    process.env.DB_PASS, {
+    process.env.DB_PASSWORD, {
         host: process.env.DB_HOST,
+        port: 4000,
         dialect: 'mysql',
-        logging: false
+        logging: false,
+        dialectOptions: {
+            ssl: {
+              require: true,
+              rejectUnauthorized: true
+            }
+        }
     });
 
 (async () => {
